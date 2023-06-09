513ba35a501fd908a53756312223ae1ac5c3e8af
imamyah <imanemachkour8@gmail.com>
2023-06-09 23:58:17 +0000

    tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    from sys import argv
+    arglen = len(argv) - 1
+    if arglen == 0:
+        print('0 arguments.')
+    elif arglen == 1:
+        print('1 argument:')
+        print('1: {}'.format(argv[1]))
+    else:
+        print('{} arguments:'.format(arglen))
+        for i in range(1, arglen):
+            print('{}: {}'.format(i, argv[i]))
